dfef98406a4afa6908472e34049e59866959d2cf
AstonDV <astondv.kms@gmail.com>
2023-01-26 18:47:25 +1000

    End Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Методы и их виды.
+
+// Вид 1
+
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+
+// Method1();
+
+
+// Вид 2
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Method2("Текст сообщения");
+
+
+// Вид 2.1
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+// Method21(count: 4, msg: "текст");
+
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+
+// Вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+
+// Вид 4.1 с использованием цикла for
+
+string Method42(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < 10; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+// string res = Method42(10, "z");
+// Console.WriteLine(res);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
